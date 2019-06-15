99de129ac80efe0ca8631cfe34fe4f7b9d3cfaf8
Carol Wanjohi <wanjohi.carol54@gmail.com>
2019-06-15 16:26:53 +0300

    create new method for generating an access token and add terminal input evaluation to determine the method used to generate an access token

--- bitbucket_api.py
+++ bitbucket_api.py
@@ -1,10 +1,24 @@
 import os
 import requests
 import json
+import sys
 from os.path import join, dirname
 from dotenv import load_dotenv
 from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
+
+def get_user_input():
+    if len(sys.argv) == 1:
+        print("Will run get_access_token")
+        return ''
+    else:
+        return sys.argv[1]
+
+def select_method_to_get_access_token(method_selected):
+    if method_selected == 'other':
+        return other_get_access_token(key)
+    else:
+        return get_access_token(username, password, key, secret_key)
 
 def get_access_token(username, password, key, secret_key):
     client = BackendApplicationClient(client_id={key})
@@ -13,6 +27,12 @@ def get_access_token(username, password, key, secret_key):
     response = oauth.fetch_token(url, username=username, password=password, auth=(key, secret_key))
     
     return response["access_token"]
+
+def other_get_access_token(key):
+    url = 'https://bitbucket.org/site/oauth2/authorize'
+    params = {'client_id':key}
+    response = requests.get(url, params)
+    return response
 
 def get_open_pull_requests(access_token):
     url = 'https://bitbucket.org/!api/2.0/pullrequests/carolwanjohi?'
@@ -31,8 +51,12 @@ if __name__ == '__main__':
     username = os.getenv('USERNAME')
     password = os.getenv('PASSWORD')
 
+    # Get user input
+    method_selected = get_user_input()
+    print('method_selected', method_selected)
+
     # Get access token
-    access_token = get_access_token(username, password, key, secret_key)
+    access_token = select_method_to_get_access_token(method_selected)    
     print('access_token', access_token)
 
     # Get Open PR
