af93c512fb8c829e715b52bf58246d43116049ae
Carol Wanjohi <wanjohi.carol54@gmail.com>
2019-06-15 15:30:30 +0300

    update call and return value of access token

--- bitbucket_api.py
+++ bitbucket_api.py
@@ -1,16 +1,23 @@
 import os
 import requests
+import json
 from os.path import join, dirname
 from dotenv import load_dotenv
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
 
-def get_access_token(key, secret_key):
+def get_access_token(username, password, key, secret_key):
     # Using variables.
+    print('username', username)
+    print('password', password)
     print('key', key)
     print('secret_key',secret_key)
+    client = BackendApplicationClient(client_id={key})
+    oauth = OAuth2Session(client=client)
     url = 'https://bitbucket.org/site/oauth2/access_token'
-    response = requests.post(url, auth=(key, secret_key))
+    response = oauth.fetch_token(url, username=username, password=password, auth=(key, secret_key))
     
-    return response.json()
+    return response["access_token"]
 
 if __name__ == '__main__':
     # Get env file where SECRET and KEY are
@@ -20,7 +27,9 @@ if __name__ == '__main__':
     # Accessing variables.
     key = os.getenv('KEY')
     secret_key = os.getenv('SECRET_KEY')
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
 
     # get access token
-    access_token = get_access_token(key, secret_key)
+    access_token = get_access_token(username, password, key, secret_key)
     print('access_token', access_token)
