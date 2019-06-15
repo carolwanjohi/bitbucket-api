4a91eb4db7154c1c6dce4e86858b51aa2cf7f077
Carol Wanjohi <wanjohi.carol54@gmail.com>
2019-06-15 15:39:35 +0300

    add function to get open pull request

--- bitbucket_api.py
+++ bitbucket_api.py
@@ -7,17 +7,18 @@ from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
 
 def get_access_token(username, password, key, secret_key):
-    # Using variables.
-    print('username', username)
-    print('password', password)
-    print('key', key)
-    print('secret_key',secret_key)
     client = BackendApplicationClient(client_id={key})
     oauth = OAuth2Session(client=client)
     url = 'https://bitbucket.org/site/oauth2/access_token'
     response = oauth.fetch_token(url, username=username, password=password, auth=(key, secret_key))
     
     return response["access_token"]
+
+def get_open_pull_requests(access_token):
+    url = 'https://bitbucket.org/!api/2.0/pullrequests/jadema?state=OPEN'
+    params = {'state': 'OPEN', 'access_token': access_token}
+    response = requests.get(url, params)
+    return response.json()
 
 if __name__ == '__main__':
     # Get env file where SECRET and KEY are
@@ -30,6 +31,10 @@ if __name__ == '__main__':
     username = os.getenv('USERNAME')
     password = os.getenv('PASSWORD')
 
-    # get access token
+    # Get access token
     access_token = get_access_token(username, password, key, secret_key)
     print('access_token', access_token)
+
+    # Get Open PR
+    open_pull_requests = get_open_pull_requests(access_token)
+    print('open_pull_requests', open_pull_requests)
